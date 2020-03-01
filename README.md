12190d332222c9c0e32c5433aa85f71097bda778
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-03-01 22:49:41 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # props-and-state
+
+# LAB - Class 28
+
+### Author: Ahmad Sawalqeh
+
+### Links and Resources
+
+- [submission PR](https://github.com/Ahmad-Sawalqeh/props-and-state/pull/1)
+- [Deploy React App]()
+- [Javascript Tests]()
+
+### Setup
+How to initialize/run your application (where applicable)
+* npm run start
+
+#### Tests
+
+- How do you run tests?
+`npm test`
+- Describe any tests that you did not complete, skipped, etc
+
+# UML
+
+![](./assesst/class28.jpeg)
--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,4 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter(), });
+
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from '../app.js';
@@ -7,7 +14,7 @@ describe('<App />', () => {
 
   it('exists at the start of the application', () => {
     let app = shallow(<App />);
-    expect(app.find('form').exists()).toBeTruthy();
+    expect(app.find('div').exists()).toBeTruthy();
   });
 
   it('renders correctly', () => {
